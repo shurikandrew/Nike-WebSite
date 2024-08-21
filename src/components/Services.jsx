b487bc64f431b175b8c299320f321ef87e68a782
shurikandrew <s26865@pjwstk.edu.pl>
2024-08-21 20:13:12 +0200

    Working version

---
+++ src/components/Services.jsx
@@ -0,0 +1,16 @@
+import {services} from "../constants";
+import ServiceCard from "./ServiceCard.jsx";
+
+const Services = () => {
+    return (
+        <section className="max-container flex justify-center flex-wrap gap-9">
+            {services.map(
+                (service) => (
+                    <ServiceCard key={service.label} {...service}/>
+                )
+            )}
+        </section>
+    );
+};
+
+export default Services;
